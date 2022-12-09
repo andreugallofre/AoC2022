a08e20f375076ac986ceb7cae4b8cd5e3a3d81d9
Andreu Gallofre <agallofr@redhat.com>
2022-12-09 14:58:50 +0100

    Refactor tail_pos calculation

--- day9/problem.py
+++ day9/problem.py
@@ -1,35 +1,15 @@
-import math, copy
+import math
 
 
 def calc_tail_pos(head: list, tail: list) -> None:
     distance = math.dist(head, tail)
+
+    diff_x = head[0] - tail[0]
+    diff_y = head[1] - tail[1]
+
     if distance >= 2:
-        if head[0] == tail[0]:
-            if head[1] > tail[1]:
-                tail[1] += 1
-            else:
-                tail[1] -= 1
-        elif head[1] == tail[1]:
-            if head[0] > tail[0]:
-                tail[0] += 1
-            else:
-                tail[0] -= 1
-
-        elif head[0] > tail[0] and head[1] > tail[1]:
-            tail[0] += 1
-            tail[1] += 1
-
-        elif head[0] > tail[0] and head[1] < tail[1]:
-            tail[0] += 1
-            tail[1] -= 1
-
-        elif head[0] < tail[0] and head[1] > tail[1]:
-            tail[0] -= 1
-            tail[1] += 1
-
-        elif head[0] < tail[0] and head[1] < tail[1]:
-            tail[0] -= 1
-            tail[1] -= 1
+        tail[0] += diff_x / abs(diff_x) if diff_x != 0 else 0
+        tail[1] += diff_y / abs(diff_y) if diff_y != 0 else 0
 
     return tail
 
@@ -37,11 +17,11 @@ def calc_tail_pos(head: list, tail: list) -> None:
 def update_tails(rope: list, visited: list) -> None:
     for i in range(1, len(rope)):
         rope[i] = calc_tail_pos(rope[i - 1], rope[i])
-        visited[i-1].append(tuple(rope[i]))
+        visited[i - 1].append(tuple(rope[i]))
 
 
 def calculate_movements(problem_input: list, visited: list) -> list:
-    rope = [[0, 0],[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+    rope = [[0, 0] for _ in range(10)]
 
     for move in problem_input:
         mov = move.split(" ")
@@ -65,7 +45,6 @@ def calculate_movements(problem_input: list, visited: list) -> list:
             for _ in range(positions):
                 rope[0][0] += 1
                 update_tails(rope, visited)
-
 
 
 def part_one(problem_input: list) -> int:
